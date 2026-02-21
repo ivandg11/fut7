22c517938f7098cb1c4406675d8f19a55631ec5d
ivandg11 <ivandominguezgomezz@gmail.com>
2026-02-20 21:02:49 -0600

    test

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://fut7-production.up.railway.app';
+const API_URL = 'https://fut7-production.up.railway.app/api';
 
 const api = axios.create({
   baseURL: API_URL,
